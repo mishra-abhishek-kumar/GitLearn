8b735caf99dc57175d7a8815a78210584f37eaa4
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-10-29 22:53:13 +0530

    added styles to list items

--- demo.js
+++ demo.js
@@ -6,3 +6,10 @@ header.style.borderBottom = 'solid 3px black';
 
 let title = document.getElementsByClassName('title');
 title[0].style.fontWeight = 'bold';
+
+let items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+
+for(var i=0; i<items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+}
