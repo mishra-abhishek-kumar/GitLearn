6c5a45dcae130a67a7c30cc3f6ba7bd28dc2fe7c
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-10-29 23:43:16 +0530

    DOM Learning

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
-console.log("Learning Git");
+const headertTitle = document.getElementById('header-title');
+headertTitle.innerText = 'Item Lists'
+
+const header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px black';
+
+let title = document.getElementsByClassName('title');
+title[0].style.fontWeight = 'bold';
